39588954c8d219f5482efb55ddc34381a0a18539
Jacopo Favaretto <jacopo.favaretto@itiszuccante.edu.it>
2026-04-05 12:39:09 +0200

    14° Commit (Barre della vita e guardie complete+bozza schermata game over)

--- Super Mega Botte Bamba 3 UltraTurbo.py
+++ Super Mega Botte Bamba 3 UltraTurbo.py
@@ -7,6 +7,23 @@ GRAVITY = 1
 PLAYER_JUMP_SPEED1 = 25
 PLAYER_JUMP_SPEED2 =18.8
 
+
+class MenuView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.BLACK)
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("SUPER MEGA BOTTE&BAMBA 2 TURBO: ADRIAN vs GABIBBO EDITION", SCREENWIDTH / 2, SCREENHEIGHT / 2 + 100,
+                         arcade.color.WHITE, font_size=33, anchor_x="center", bold=True)
+        arcade.draw_text("Premi INVIO per iniziare", SCREENWIDTH / 2, SCREENHEIGHT / 2,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ENTER or key == arcade.key.RETURN:
+            battle_view = Giochino()
+            battle_view.setup()
+            self.window.show_view(battle_view)
+
+
 class Vita1(arcade.Sprite):
     def __init__(self):
         super().__init__()
@@ -18,10 +35,19 @@ class Vita1(arcade.Sprite):
         self.VitaAttuale = self.MaxVita
 
     def draw(self):
-        Vita_Width = (self.VitaAttuale / self.MaxVita) *self.width
         arcade.draw_lbwh_rectangle_filled(self.center_x,self.center_y,self.width,self.height,arcade.color.BLACK)
-        x_offset = (self.width - Vita_Width) / 2
-        arcade.draw_lbwh_rectangle_filled(self.center_x - x_offset,self.center_y,Vita_Width,self.height,arcade.color.GREEN)
+        Vita_Width = (self.VitaAttuale / self.MaxVita) *self.width
+        if Vita_Width > 0:
+            arcade.draw_lbwh_rectangle_filled(self.center_x, self.center_y, Vita_Width, self.height, arcade.color.GREEN)
+        testo_vita = f"{int(self.VitaAttuale)} / {int(self.MaxVita)}"
+        arcade.draw_text(testo_vita, 
+                         self.center_x + 5,               
+                         self.center_y + self.height / 2, 
+                         arcade.color.WHITE, 
+                         font_size=11, 
+                         anchor_x="left",                 
+                         anchor_y="center",
+                         bold=True)
 
 
 class Vita2(arcade.Sprite):
@@ -35,10 +61,19 @@ class Vita2(arcade.Sprite):
         self.VitaAttuale = self.MaxVita
 
     def draw(self):
-        Vita_Width = (self.VitaAttuale / self.MaxVita) *self.width
         arcade.draw_lbwh_rectangle_filled(self.center_x,self.center_y,self.width,self.height,arcade.color.BLACK)
-        x_offset = (self.width - Vita_Width) / 2
-        arcade.draw_lbwh_rectangle_filled(self.center_x - x_offset,self.center_y,Vita_Width,self.height,arcade.color.GREEN)
+        Vita_Width = (self.VitaAttuale / self.MaxVita) *self.width
+        if Vita_Width > 0:
+            arcade.draw_lbwh_rectangle_filled(self.center_x, self.center_y, Vita_Width, self.height, arcade.color.GREEN)
+        testo_vita = f"{int(self.VitaAttuale)} / {int(self.MaxVita)}"
+        arcade.draw_text(testo_vita, 
+                         self.center_x + self.width - 5,
+                         self.center_y + self.height / 2, 
+                         arcade.color.WHITE, 
+                         font_size=11, 
+                         anchor_x="right",               
+                         anchor_y="center",
+                         bold=True)
 
 
 class Attacco1(arcade.Sprite):
@@ -47,7 +82,7 @@ class Attacco1(arcade.Sprite):
         self.center_x = player.center_x
         self.center_y = player.center_y
         self.distanza_percorsa = 0
-        self.distanza_massima = 250
+        self.distanza_massima = 175
         self.speed = 15
     
 
@@ -63,7 +98,7 @@ class Attacco2(arcade.Sprite):
         super().__init__("./pugno.png",scale= 0.1)
         self.center_x = player.center_x
         self.center_y = player.center_y
-        self.distanza_percorsa = 250
+        self.distanza_percorsa = 175
         self.distanza_massima = 0
         self.speed = 15
         
@@ -78,7 +113,7 @@ class Attacco2(arcade.Sprite):
 class Giochino(arcade.Window):
     
     def __init__(self):
-        super().__init__(SCREENWIDTH,SCREENHEIGHT,"Super Mega Botte&Bamba 3 Ultra Turbo")
+        super().__init__(SCREENWIDTH,SCREENHEIGHT,"Super Botte&Bamba 2 Turbo: ADRIAN vs GABIBBO EDITION")
     
         self.lista_Gabibbo= arcade.SpriteList()
         self.lista_Adrian= arcade.SpriteList()
@@ -90,16 +125,21 @@ class Giochino(arcade.Window):
         self.muovi_destraGabibbo= False
         self.muovi_sinistraAdrian = False
         self.muovi_sinistraGabibbo= False
-        self.attackAdrian = False
         self.guardAdrian = False
-        self.attackGabibbo =False
         self.guardGabibbo = False
+        self.adrian_morto = False
+        self.gabibbo_morto = False
         self.lista_potere = arcade.SpriteList()
         self.setup()
 
 
 
     def setup(self):
+        self.lista_Gabibbo.clear()
+        self.lista_Adrian.clear()
+        self.wall_list.clear()
+        self.lista_potere.clear()
+
 
         self.pavimento =arcade.Sprite("./Pavimento1.jpg")
         self.wall_list.append(self.pavimento)
@@ -114,6 +154,7 @@ class Giochino(arcade.Window):
         self.sprite_gabibbo.center_y = 200
         self.sprite_gabibbo.scale = 0.6
         self.lista_Gabibbo.append(self.sprite_gabibbo)
+        self.texture_gabibbo_morto = arcade.load_texture("pugno.png")
 
 
         self.sprite_adrian=arcade.Sprite("Adrian.png")
@@ -121,6 +162,12 @@ class Giochino(arcade.Window):
         self.sprite_adrian.center_y = 250
         self.sprite_adrian.scale = 0.5
         self.lista_Adrian.append(self.sprite_adrian)
+        self.texture_adrian_morto = arcade.load_texture("pugno.png")
+
+        self.immagine_fine1 = arcade.load_texture("./pugno.png")
+        self.immagine_fine2 = arcade.load_texture("./pugno.png")
+        self.immagine_fine3 = arcade.load_texture("./pugno.png")
+
 
 
         self.physics_engine_gabibbo = arcade.PhysicsEnginePlatformer(self.sprite_gabibbo, walls=self.wall_list, gravity_constant=GRAVITY)
@@ -132,61 +179,82 @@ class Giochino(arcade.Window):
 
 
     def on_update(self, delta_time: float) -> bool | None:
-        if self.muovi_destraAdrian:
-            self.sprite_adrian.center_x += 10
-        if self.muovi_sinistraAdrian:
-            self.sprite_adrian.center_x -= 10
+        if self.muovi_destraAdrian and not self.guardAdrian:
+            self.sprite_adrian.center_x += 7.5
+        if self.muovi_sinistraAdrian and not self.guardAdrian:
+            self.sprite_adrian.center_x -= 7.5
+
         if self.sprite_adrian.center_x <= 0:
             self.sprite_adrian.center_x = 1
+
         if self.sprite_gabibbo.center_x <= 0:
             self.sprite_gabibbo.center_x = 1
+
         if self.sprite_adrian.center_x >= 1272:
             self.sprite_adrian.center_x = 1271
+
         if self.sprite_gabibbo.center_x >= 1272:
             self.sprite_gabibbo.center_x = 1271
-        if self.muovi_destraGabibbo:
-            self.sprite_gabibbo.center_x += 10
-        if self.muovi_sinistraGabibbo:
-            self.sprite_gabibbo.center_x -=10
+
+        if self.muovi_destraGabibbo and not self.guardGabibbo:
+            self.sprite_gabibbo.center_x += 7.5
+        if self.muovi_sinistraGabibbo and not self.guardGabibbo:
+            self.sprite_gabibbo.center_x -= 7.5
+
         if self.sprite_adrian.center_y >= 600:
             self.sprite_adrian.center_y = 600
+
         if self.sprite_gabibbo.center_y >= 600:
             self.sprite_gabibbo.center_y = 600
 
+
+        if self.BarraVitaGabibbo.VitaAttuale <= 0:
+            self.BarraVitaGabibbo.VitaAttuale = 0
+            self.gabibbo_morto = True
+        if self.gabibbo_morto:
+            self.sprite_gabibbo.texture = self.texture_gabibbo_morto
+            self.sprite_gabibbo.center_y -= 20
+
+
+        if self.BarraVitaAdrian.VitaAttuale <= 0:
+            self.BarraVitaAdrian.VitaAttuale = 0
+            self.adrian_morto = True
+        if self.adrian_morto:
+            self.sprite_adrian.texture = self.texture_adrian_morto
+            self.sprite_adrian.center_y -= 20
+
+
+        distanza_x = abs(self.sprite_adrian.center_x - self.sprite_gabibbo.center_x)
+        distanza_minima = 70
+        if distanza_x < distanza_minima:
+            if self.sprite_adrian.center_x < self.sprite_gabibbo.center_x:
+                if self.muovi_destraAdrian:
+                    self.sprite_adrian.center_x -= 7.5
+                if self.muovi_sinistraGabibbo:
+                    self.sprite_gabibbo.center_x += 7.5
         
         self.physics_engine_adrian.update()
         self.physics_engine_gabibbo.update()
+
         self.lista_potere.update()
 
 
         for attacco in self.lista_potere:
             if isinstance(attacco,Attacco1):
-                collisione = arcade.check_for_collision(attacco,self.sprite_gabibbo)
-                if collisione:
+                if arcade.check_for_collision(attacco, self.sprite_gabibbo):
+                    danno = 15 if not self.guardGabibbo else 0
+                    self.BarraVitaGabibbo.VitaAttuale -= danno
                     attacco.remove_from_sprite_lists()
-                    self.BarraVitaGabibbo.VitaAttuale -= 20
-                if self.BarraVitaGabibbo.VitaAttuale <= 0:
-                    self.BarraVitaGabibbo.VitaAttuale = 0
-                if self.guardGabibbo:
-                    self.BarraVitaGabibbo.VitaAttuale += 20
-                    if self.BarraVitaGabibbo.VitaAttuale > self.BarraVitaGabibbo.MaxVita:
-                        self.BarraVitaGabibbo.VitaAttuale = self.BarraVitaGabibbo.MaxVita
-
-        for attacco in self.lista_potere:
-            if isinstance(attacco,Attacco2):
-                collisione = arcade.check_for_collision(attacco,self.sprite_adrian)
-                if collisione:
+                    continue
+            elif isinstance(attacco, Attacco2):
+                if arcade.check_for_collision(attacco, self.sprite_adrian):
+                    danno = 15 if not self.guardAdrian else 0
+                    self.BarraVitaAdrian.VitaAttuale -= danno
                     attacco.remove_from_sprite_lists()
-                    self.BarraVitaAdrian.VitaAttuale -= 20
-        if self.BarraVitaAdrian.VitaAttuale <= 0:
-            self.BarraVitaAdrian.VitaAttuale = 0
-        if self.guardAdrian:
-            self.BarraVitaAdrian.VitaAttuale += 20
-            if self.BarraVitaAdrian.VitaAttuale > self.BarraVitaAdrian.MaxVita:
-                self.BarraVitaAdrian.VitaAttuale = self.BarraVitaAdrian.MaxVita
-
-
-        
+                    continue
+        if self.BarraVitaGabibbo.VitaAttuale <= 0: self.BarraVitaGabibbo.VitaAttuale = 0
+        if self.BarraVitaAdrian.VitaAttuale <= 0: self.BarraVitaAdrian.VitaAttuale = 0
+            
     def on_draw(self):
         self.clear()
         arcade.draw_texture_rect(
@@ -197,11 +265,23 @@ class Giochino(arcade.Window):
         self.lista_Gabibbo.draw()
         self.lista_Adrian.draw()
         #self.wall_list.draw()
-        self.lista_potere.draw()
+        #self.lista_potere.draw()
         self.BarraVitaAdrian.draw()
         self.BarraVitaGabibbo.draw()
+        if self.gabibbo_morto:
+            arcade.draw_texture_rect(
+                self.immagine_fine1, 
+                arcade.XYWH(SCREENWIDTH / 2, SCREENHEIGHT / 2, 400, 200))
+        if self.adrian_morto:
+            arcade.draw_texture_rect(
+                self.immagine_fine2, 
+                arcade.XYWH(SCREENWIDTH / 2, SCREENHEIGHT / 2, 400, 200))
+        if self.gabibbo_morto and self.adrian_morto:
+            arcade.draw_texture_rect(
+                self.immagine_fine3, 
+                arcade.XYWH(SCREENWIDTH / 2, SCREENHEIGHT / 2, 400, 200))
+            
  
-
     def on_key_press(self, key, modifiers):
         if key == arcade.key.W:
             if self.physics_engine_adrian.can_jump():
@@ -218,13 +298,11 @@ class Giochino(arcade.Window):
         if key == arcade.key.LEFT:
             self.muovi_sinistraGabibbo = True
         if key == arcade.key.X:
-            self.attackAdrian = True
             attacco = Attacco1(self.sprite_adrian)
             self.lista_potere.append(attacco)
         if key == arcade.key.C:
             self.guardAdrian = True
         if key == arcade.key.N:
-            self.attackGabibbo = True
             attacco = Attacco2(self.sprite_gabibbo)
             self.lista_potere.append(attacco)
         if key == arcade.key.M:
@@ -256,7 +334,9 @@ class Giochino(arcade.Window):
 
 
 def main():
-    gioco = Giochino()
+    window = arcade.Window(SCREENWIDTH, SCREENHEIGHT, "Super Botte & Bamba 2 Turbo: ADRIAN vs GABIBBO EDITION")
+    menu_iniziale = MenuView()
+    window.show_view(menu_iniziale)
     arcade.run()
 
 
